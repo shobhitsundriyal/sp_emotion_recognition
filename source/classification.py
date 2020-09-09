36062cf2dd1a5c37bccdd7ff1f6dca5d1072acec
unknown <shobhitsundriyal0170@gmail.com>
2020-09-09 20:30:59 +0530

    dummy

---
+++ source/classification.py
@@ -0,0 +1,23 @@
+
+
+
+'''
+The following dummy code for demonstration.
+'''
+
+
+def train_a_model(trainfile):
+    '''
+    :param trainfile:
+    :return:
+    '''
+    pass
+
+
+
+def test_the_model(testfile):
+    '''
+
+    :param testfile:
+    :return:  a list of predicted values in same order of
+    '''
